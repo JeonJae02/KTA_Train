835c70ab7a678623de31e6e88f0cac7b18c3e62a
JeonJae02 <wjswogud02@gmail.com>
2026-04-25 18:26:44 +0900

    add requirements txt.

--- train_pump_val.py
+++ train_pump_val.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from Log_Extractor import LogExtractor
 from pump_config import PUMP_TO_TANK
